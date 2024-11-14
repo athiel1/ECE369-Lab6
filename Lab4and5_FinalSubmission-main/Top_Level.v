746c34362af6da5f279f64534985b4d7e2a4b003
athiel1 <157844495+athiel1@users.noreply.github.com>
2024-11-14 12:38:02 -0700

    Update Top_Level.v

--- Lab4and5_FinalSubmission-main/Top_Level.v
+++ Lab4and5_FinalSubmission-main/Top_Level.v
@@ -92,12 +92,14 @@ module Top_Level(Reset, ClkOut, PC_pin_out, write_data_pin);
                     //RegWrite_out_ID, MemtoReg_ID, Branch_ID, MemRead_ID, MemWrite_ID, RegDst_ID, ALUOp_ID, 
                     //ALUSrc_ID, PCAddResult_out_ID, ReadData1_out_ID, ReadData2_out_ID, SignExtResult_ID, 
                     //rt_ID, rd_ID, JR_ID, size_ID, Clk_in, write_data_pin, special_rt_ID, j_and_jal_ID,
-                    //ALUAddResult_ID, rs_IFID, rt_IFID, MemRead_IDEX, RegisterRt_IDEX, RegisterRd_IDEX, PCWrite_ID, IFIDWrite_ID);
+                    //ALUAddResult_ID, /*rs_IFID, rt_IFID,*/ MemRead_IDEX, RegisterRt_IDEX, RegisterRd_IDEX, PCWrite_ID, IFIDWrite_ID,
+                    //Branch_IDEX, JR_IDEX, j_and_jal_IDEX);
     stage_ID top3(PCAdder_out_IFID, Instruction_out_IFID, RegWrite_out_MEMWB, mux2_result_out_MEMWB, mux3_result_WB,
                     RegWrite_out_ID, MemtoReg_ID, Branch_ID, MemRead_ID, MemWrite_ID, RegDst_ID, ALUOp_ID, 
                     ALUSrc_ID, PCAddResult_out_ID, ReadData1_out_ID, ReadData2_out_ID, SignExtResult_ID, 
                     rt_ID, rd_ID, JR_ID, size_ID, ClkOut, write_data_pin, special_rt_ID, j_and_jal_ID,
-                    ALUAddResult_ID, /*rs_ID, rt_ID,*/ MemRead_out_IDEX, rt_out_IDEX, rd_out_IDEX, PCWrite_ID, IFIDWrite_ID);
+                    ALUAddResult_ID, /*rs_ID, rt_ID,*/ MemRead_out_IDEX, rt_out_IDEX, rd_out_IDEX, PCWrite_ID, IFIDWrite_ID,
+                    Branch_out_IDEX, JR_out_IDEX, j_and_jal_out_IDEX);
                     
     //ID_EX(PCAddResult_in_IDEX, ReadData1_in_IDEX, ReadData2_in_IDEX, signExtend_in_IDEX, rt_in_IDEX, 
     //            rd_in_IDEX, RegWrite_in_IDEX, MemtoReg_in_IDEX, Branch_in_IDEX, MemRead_in_IDEX, 
@@ -117,10 +119,10 @@ module Top_Level(Reset, ClkOut, PC_pin_out, write_data_pin);
                 
                 
      //stage_EX (RegWrite_in_EX, MemtoReg_in_EX, Branch_in_EX, MemRead_in_EX, MemWrite_in_EX, RegDst_EX, 
-     //            ALUOp_EX, ALUSrc_EX, PCAddResult_EX, ReadData1_EX, ReadData2_in_EX, SignExtResult_EX, 
-     //            rt_EX, rd_EX, RegWrite_out_EX, MemtoReg_out_EX, 
-     //            Branch_out_EX, MemRead_out_EX, MemWrite_out_EX, ALUAddResult_EX, Zero_EX, ALUResult_EX, 
-     //            ReadData2_out_EX, mux2_result_EX, size_in_EX, size_out_EX, Clk_in);
+                 //ALUOp_EX, ALUSrc_EX, PCAddResult_EX, ReadData1_EX, ReadData2_in_EX, SignExtResult_EX, 
+                 //rt_EX, rd_EX, RegWrite_out_EX, MemtoReg_out_EX, 
+                 //Branch_out_EX, MemRead_out_EX, MemWrite_out_EX, ALUAddResult_EX, Zero_EX, ALUResult_EX, 
+                 //ReadData2_out_EX, mux2_result_EX, size_in_EX, size_out_EX, JR_in_EX, JR_out_EX, special_rt_EX, j_and_jal_in_EX, j_and_jal_out_EX);
      stage_EX top5(RegWrite_out_IDEX, MemtoReg_out_IDEX, Branch_out_IDEX, MemRead_out_IDEX, MemWrite_out_IDEX, RegDst_out_IDEX,
                     ALUOp_out_IDEX, ALUSrc_out_IDEX, PCAddResult_out_IDEX, ReadData1_out_IDEX, ReadData2_out_IDEX, signExtend_out_IDEX,
                     rt_out_IDEX, rd_out_IDEX, RegWrite_out_EX, MemtoReg_out_EX, 
@@ -130,17 +132,17 @@ module Top_Level(Reset, ClkOut, PC_pin_out, write_data_pin);
                     
                     
      //EX_MEM(MemWrite_in_EXMEM, MemRead_in_EXMEM, Branch_in_EXMEM, MemtoReg_in_EXMEM, 
-     //           RegWrite_in_EXMEM, ALUAddResult_in_EXMEM, Zero_in_EXMEM, ALUResult_in_EXMEM, 
-     //           ReadData2_in_EXMEM, mux2_Result_in_EXMEM,
-     //           MemWrite_out_EXMEM, MemRead_out_EXMEM, Branch_out_EXMEM, MemtoReg_out_EXMEM, 
-     //           RegWrite_out_EXMEM, ALUAddResult_out_EXMEM, Zero_out_EXMEM, ALUResult_out_EXMEM, 
-     //           ReadData2_out_EXMEM, mux2_Result_out_EXMEM,
-     //           size_in_EXMEM, size_out_EXMEM, Clk_in, Rst);
+                //RegWrite_in_EXMEM, /*ALUAddResult_in_EXMEM,*/ Zero_in_EXMEM, ALUResult_in_EXMEM, 
+                //ReadData2_in_EXMEM, mux2_Result_in_EXMEM,
+                //MemWrite_out_EXMEM, MemRead_out_EXMEM, Branch_out_EXMEM, MemtoReg_out_EXMEM, 
+                //RegWrite_out_EXMEM, /*ALUAddResult_out_EXMEM,*/ Zero_out_EXMEM, ALUResult_out_EXMEM, 
+                //ReadData2_out_EXMEM, mux2_Result_out_EXMEM,
+                //size_in_EXMEM, size_out_EXMEM, Clk_in, Rst, JR_in_EXMEM, JR_out_EXMEM, j_and_jal_in_EXMEM, j_and_jal_out_EXMEM);
      EX_MEM top6(MemWrite_out_EX, MemRead_out_EX, Branch_out_EX, MemtoReg_out_EX, 
-                RegWrite_out_EX, /*ALUAddResult_EX,*/ Zero_EX, ALUResult_EX, 
+                RegWrite_out_EX, Zero_EX, ALUResult_EX, 
                 ReadData2_out_EX, mux2_result_EX, 
                 MemWrite_out_EXMEM, MemRead_out_EXMEM, Branch_out_EXMEM, MemtoReg_out_EXMEM, 
-                RegWrite_out_EXMEM, /*ALUAddResult_out_EXMEM,*/ Zero_out_EXMEM, ALUResult_out_EXMEM, 
+                RegWrite_out_EXMEM, Zero_out_EXMEM, ALUResult_out_EXMEM, 
                 ReadData2_out_EXMEM, mux2_Result_out_EXMEM,
                 size_out_EX, size_out_EXMEM, ClkOut, Reset, JR_out_EX, JR_out_EXMEM, j_and_jal_out_EX, j_and_jal_out_EXMEM);
                 
