4e657a8955b1196e7c28d548263a7b18aa289cba
athiel1 <157844495+athiel1@users.noreply.github.com>
2024-11-13 17:53:11 -0700

    Update Hazard.v

--- Lab4and5_FinalSubmission-main/Hazard.v
+++ Lab4and5_FinalSubmission-main/Hazard.v
@@ -20,30 +20,43 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module Hazard(MemRead, RegisterRt, RegisterRd, rs, rt, IFIDWrite, PCWrite, hazardControl);
+module Hazard(MemRead, RegisterRt, RegisterRd, rs, rt, IFIDWrite, PCWrite, hazardControl, Clk);
     input MemRead;
     input [4:0] RegisterRt;
     input [4:0] RegisterRd;
     input [4:0] rs;
     input [4:0] rt;
+    input Clk;
     
     output reg IFIDWrite;
     output reg PCWrite;
     output reg hazardControl;
     
+    initial begin
+        PCWrite <= 1;
+        IFIDWrite <= 1;
+        hazardControl <= 0;
+        repeat(2) @(posedge Clk);
+    end
+    
     always @(*) begin
+        PCWrite <= 1;
+        IFIDWrite <= 1;
+        hazardControl <= 0;
+        
         if (MemRead) begin
             if ((RegisterRt == rs) | (RegisterRt == rt)) begin
                 PCWrite <= 0;
-                IFIDWrite <= 1;
+                IFIDWrite <= 0;
                 hazardControl = 1;
             end
         end
         else if ((RegisterRd == rs) | (RegisterRd == rt)) begin
-                PCWrite <= 0;
-                IFIDWrite <= 1;
+                PCWrite <= 0; 
+                IFIDWrite <= 0; 
                 hazardControl = 1;
         end
+       
     end 
 
 
