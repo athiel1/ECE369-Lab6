032a58545e74a0dd8296be83abfecc646a10f873
athiel1 <157844495+athiel1@users.noreply.github.com>
2024-11-13 18:05:47 -0700

    Update IF_ID.v

--- Lab4and5_FinalSubmission-main/IF_ID.v
+++ Lab4and5_FinalSubmission-main/IF_ID.v
@@ -2,12 +2,13 @@
 
 
 module IF_ID(PCAdder_in_IFID, Instruction_in_IFID, PCAdder_out_IFID, Instruction_out_IFID,
-            Clk_in, Rst); 
+            Clk_in, Rst, IFIDWrite); 
   
   input [31:0] PCAdder_in_IFID;
   input [31:0] Instruction_in_IFID;
   input Clk_in;
   input Rst;
+  input IFIDWrite;
 
   output reg [31:0] PCAdder_out_IFID;
   output reg [31:0] Instruction_out_IFID;
@@ -18,8 +19,10 @@ module IF_ID(PCAdder_in_IFID, Instruction_in_IFID, PCAdder_out_IFID, Instruction
         Instruction_out_IFID <= 0;
     end
     else if (Clk_in) begin
-        PCAdder_out_IFID <= PCAdder_in_IFID;
-        Instruction_out_IFID <= Instruction_in_IFID;
+        if (IFIDWrite) begin
+            PCAdder_out_IFID <= PCAdder_in_IFID;
+            Instruction_out_IFID <= Instruction_in_IFID;
+        end
     end
   end
 
